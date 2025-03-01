6037bca3c5eb545d97c788d07eb4c1b3fef73389
vellun <moonpassiflora@gmail.com>
2025-03-01 17:22:20 +0300

    [feat] init orders

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from api.auth.views import router as auth_router
+from api.orders.views import router as orders_router
 from api.ping.views import router as ping_router
 
 router = APIRouter(prefix="/api")
 
 router.include_router(ping_router)
 router.include_router(auth_router)
+router.include_router(orders_router)
---
+++ app/api/orders/views.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from starlette import status
+
+from api.auth import errors
+from api.auth.depends import SessionDep, UserDep
+from core.models.jwt import AccessToken, RefreshToken, TokenCollection
+from core.models.user import UserCreate, UserLogin, UserPublic
+from core.schemas import DetailScheme
+from core.store import store
+
+router = APIRouter(prefix="/orders", tags=["Заказы"])
+
+
+@router.get(
+    "/",
+    summary="Заказы",
+    response_description="Заказы пользователя",
+)
+async def get_orders(
+    user: UserDep,
+    session: SessionDep,
+    offset: int = 1,
+    page: int = 10,
+    fuel_type: str | None = None,
+    depot: str | None = None,
+):
+    return await store.order_accessor.get_all_orders(
+        session=session,
+        offset=offset,
+        page=page,
+        fuel_type=fuel_type,
+        depot=depot,
+    )
+
+
+@router.get(
+    "/{order_id}",
+    summary="Заказ",
+    response_description="Заказ",
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Заказ с id {order_id} не найден",
+            "model": DetailScheme,
+        },
+    },
+)
+async def get_order_by_id(user: UserDep) -> UserPublic:
+    return store.order_accessor.get_order_by_id(user_id=user.id)
+
+
+@router.post(
+    "/",
+    summary="Создание заказ",
+    response_description="Создание заказа",
+)
+async def order_create(
+    user_in: UserCreate,
+    request: Request,
+    background_tasks: BackgroundTasks,
+    session: SessionDep,
+) -> None:
+    user_id = await store.order_accessor.create_order(session=session, user_in=user_in)
+    await session.commit()
+
+    return None
